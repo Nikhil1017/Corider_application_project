13baea68602ea87d60fb8a8bca3f9c2e8aee1c7a
Nikhil1017 <115337705+Nikhil1017@users.noreply.github.com>
2023-04-16 18:22:21 +0530

    Restful apis added

--- app.py
+++ app.py
@@ -1,51 +1,46 @@
 from flask import Flask
+from flask_restful import Resource, Api, reqparse
 from flask_pymongo import PyMongo
-from flask import jsonify, request, json
-from bson import ObjectId, json_util
-
+from bson import ObjectId
 
 app = Flask(__name__)
+api = Api(app)
+
 app.config["MONGO_URI"] = "mongodb+srv://NikhilKrishna:UaUcC7NE1SeP2qJB@cluster0.arkj1sx.mongodb.net/test?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
-@app.route('/users', methods=['POST'])
-def user_resource():
-    id = request.json['id']
-    name = request.json['name']
-    email = request.json['email']
-    password = request.json['password']
-    mongo.db.users.insert_one({'id': id,'name': name, 'email': email, 'password': password})
-    return jsonify({'message': 'user resource created successfully!'})
+class User(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('id', type=str, required=True)
+        self.reqparse.add_argument('name', type=str, required=True)
+        self.reqparse.add_argument('email', type=str, required=True)
+        self.reqparse.add_argument('password', type=str, required=True)
+        super(User, self).__init__()
 
-@app.route('/users')
-def all_users():
-    users = mongo.db.users.find()
-    userlist = []
-    for user in users:
-        user['_id'] = str(user['_id'])
-        userlist.append(user)
-    return {'users': userlist}
+    def post(self):
+        args = self.reqparse.parse_args()
+        mongo.db.users.insert_one(args)
+        return {'message': 'user resource created successfully!'}, 201
 
-@app.route('/users/<userid>', methods=['GET'])
-def userinfo(userid):
-    user = mongo.db.users.find_one({'_id':ObjectId(userid)})
-    return jsonify(json.loads(json_util.dumps(user)))
+    def get(self):
+        users = mongo.db.users.find()
+        userlist = []
+        for user in users:
+            user['_id'] = str(user['_id'])
+            userlist.append(user)
+        return {'users': userlist}, 200
 
+    def put(self, userid):
+        args = self.reqparse.parse_args()
+        mongo.db.users.update_one({'_id': ObjectId(userid)}, {'$set': args})
+        return {'message': 'user details updated'}, 200
 
-@app.route('/users/<userid>', methods=['PUT'])
-def update(userid):
-    id = request.json['id']
-    name = request.json['name']
-    email = request.json['email']
-    password = request.json['password']
-    mongo.db.users.update_one({'_id': ObjectId(userid)}, {'$set':{'id': id,'name': name, 'email': email, 'password': password}})
-    return jsonify({'message': 'user details updated'})
-
-@app.route('/users/<userid>', methods=['DELETE'])
-def delete (userid):
-   mongo.db.users.delete_one({'_id': ObjectId(userid)})
-   return jsonify({'message': 'user has been deleted successfully'})
-       
+    def delete(self, userid):
+        mongo.db.users.delete_one({'_id': ObjectId(userid)})
+        return {'message': 'user has been deleted successfully'}, 200
+    
+api.add_resource(User, '/users', '/users/<userid>')  
 
 if __name__ == '__main__':
     app.run(debug=True)
